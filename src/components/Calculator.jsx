88c70c15e0fe244278574d140528beee8aee124b
rachit1905 <96972170+rachit1905@users.noreply.github.com>
2023-03-28 21:19:43 +0530

    Added Basic UI framework

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import "./calc.css";
 
 const Calculator = () => {
   return (
-    <div>Calculator</div>
-  )
-}
+    <div className="calc">
+      <div className="display">
+        <p></p>
+      </div>
+      <div className="controls">
+        <button>7</button>
+        <button>8 </button>
+        <button>9</button>
+        <button>+</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>-</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>*</button>
+        <button>0</button>
+        <button>erase</button>
+        <button>=</button>
+        <button>/</button>
+      </div>
+    </div>
+  );
+};
 
-export default Calculator
+export default Calculator;
